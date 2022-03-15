c98f8eb4656d894d848790e811ca10506e8bee5b
isorokina <ijasorokina@inbox.lv>
2022-03-15 17:29:40 +0200

    piezim

---
+++ speele.js
@@ -0,0 +1,15 @@
+    
+    
+    
+   document.querySelector('.virsraksts').innerHTML='Vārds:'+vaards+','+vecums+','+regions //konkrēta cilvēka ieraksts
+    let pieci=['kaķis','skola','ciems','bilde','lapsa','lācis','vilks', 'putns', 'glāze', 'krūze', 'torte']
+    let sesi=['lakats','vāvere','rinkis','vasara','priede','kafija','ķirbis', 'durvis']
+    let septini=['pilsēta','plaukts','numurs','burkāni','lineāls','ārzemes','grāmata']
+    let astoni=['pulveris','aploksne','rokturis','austiņas','kabinets','tālrunis','traktors']
+    let rand5=Math.floor(Math.random()*((pieci.length -1)+1))
+    let arr5=str.split('')
+    let rand6=Math.floor(Math.random()*((sesi.length -1)+1))
+    let arr5=str.split('')
+    let rand7=Math.floor(Math.random()*((septini.length -1)+1))
+    let arr5=str.split('')
+    let rand8=Math.floor(Math.random()*((astoni.length -1)+1))
